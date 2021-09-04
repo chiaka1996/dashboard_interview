b798fff435884efc0c9347eead62e31a53db89d5
newton <chiakajunior@yahoo.com>
2021-09-04 02:58:46 +0100

    made the dashboard mobile responsive

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const list = document.querySelectorAll('.list');
 const listItem = document.querySelectorAll('.list-item');
+const hamburger = document.querySelector('.hamburger');
+const bar_one = document.querySelector('#bar_one');
+const bar_two = document.querySelector('#bar_two');
+const bar_three = document.querySelector('#bar_three');
+const sidebar1 = document.querySelector('.sidebar1');
+const sidebar2 = document.querySelector('.sidebar2');
 
 let draggedItem = null;
+let showSideBar = true;
 
 console.log(listItem);
 console.log(list);
@@ -41,3 +48,26 @@ for(let i=0; i < listItem.length; i++){
         })
     }
 }
+
+hamburger.addEventListener('click', (e) => {
+
+    if(showSideBar){
+    bar_one.classList.add('bar_one');
+    bar_two.classList.add('bar_two');
+    bar_three.classList.add('bar_three');
+    sidebar1.classList.add('addsidebar1');
+    sidebar2.classList.add('addsidebar2');
+    showSideBar = false;
+    return;
+    }
+
+    if(!showSideBar){
+        bar_one.classList.remove('bar_one');
+        bar_two.classList.remove('bar_two');
+        bar_three.classList.remove('bar_three');
+        sidebar1.classList.remove('addsidebar1');
+        sidebar2.classList.remove('addsidebar2');
+        showSideBar = true;
+        return;
+    }
+})
